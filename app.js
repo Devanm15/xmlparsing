14e912a956545d980f78d4998d31befca8120b19
Devanm15 <devan.mckay@gmail.com>
2020-09-30 14:05:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+
+// Receive xml data from parser
+http.createServer(function (req, res) {
+    var body = "";
+   
+    req.on('data', function (chunk) {
+      body += chunk; 
+    })
+    // after data is received appendFile will add to existing file with that name or create the file if it doesn't exist already, due to async nature will throw 'ERR_STREAM_WRITE_AFTER_END'
+    // but still creates and appends the xml file
+    req.on('end', function () {
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+        fs.appendFile("blog.xml", body,  function (err) {
+          if (err) throw err;
+          res.write('File created!');
+        })
+      res.writeHead(200);
+      res.end(body);
+    })
+}).listen(8080)
+
